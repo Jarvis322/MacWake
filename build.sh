9955e281a04f57dbecf5b37268196b887ca17b1b
Yigit Can Polat <yigitcpolat@gmail.com>
2026-06-28 11:04:55 +0300

    fix: Dynamic Island alerts now auto-dismiss; turning off calibration stops it and clears the stuck island notification

--- build.sh
+++ build.sh
@@ -59,9 +59,9 @@ cat <<EOF > "${CONTENTS_DIR}/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.18</string>
+    <string>1.19</string>
     <key>CFBundleVersion</key>
-    <string>19</string>
+    <string>20</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>LSApplicationCategoryType</key>
