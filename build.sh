cea7daab4461b34764fa1f5040f921087eed646d
Yigit <yigitcpolat@gmail.com>
2026-08-07 21:38:13 +0300

    release: v1.46 — true battery health reading

--- build.sh
+++ build.sh
@@ -54,9 +54,9 @@ cat <<EOF > "${CONTENTS_DIR}/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.45</string>
+    <string>1.46</string>
     <key>CFBundleVersion</key>
-    <string>51</string>
+    <string>52</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>LSApplicationCategoryType</key>
