c2ce23ec9c73a6d015d650b663de81dcaadb7d54
Yigit <yigitcpolat@gmail.com>
2026-08-09 19:58:34 +0300

    release: v1.62 — restore localized copy-button labels, refresh fan info before reporting

--- build.sh
+++ build.sh
@@ -54,9 +54,9 @@ cat <<EOF > "${CONTENTS_DIR}/Info.plist"
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>1.61</string>
+    <string>1.62</string>
     <key>CFBundleVersion</key>
-    <string>67</string>
+    <string>68</string>
     <key>LSMinimumSystemVersion</key>
     <string>14.0</string>
     <key>LSApplicationCategoryType</key>
